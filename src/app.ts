ff5be84c378be71dfb6cca50e728fedc4b61ad39
adevnamednathi <maphanga.ng@gmail.com>
2022-04-13 00:47:20 +0200

    Minor clean ups

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,23 @@
 import * as express from 'express'
 import { SchedulerBuilder } from './business/scheduler/scheduler-builder'
-import { schedule, ScheduledTask } from 'node-cron'
 import { GlobalContainer } from './container/global-container'
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-const gloabContainer: GlobalContainer = new GlobalContainer()
-const schedulerBuilder: SchedulerBuilder = new SchedulerBuilder(gloabContainer)
+const globalContainer: GlobalContainer = new GlobalContainer()
+const schedulerBuilder: SchedulerBuilder = new SchedulerBuilder(globalContainer)
 
 schedulerBuilder.startSchedulers()
 
 app.get('/', (req, res) => {
-    res.send('Hello World webscrapper!');
-});
+  res.send('Hello World webscrapper!')
+})
 
 app.get('/job-details', (req, res) => {
-    res.send(schedulerBuilder.getJobDetails())
+  res.send(schedulerBuilder.getJobDetails())
 })
 
 app.listen(port, () => {
-    return console.log(`Express is listening at http://localhost:${port}`)
-});
+  return console.log(`Express is listening at http://localhost:${port}`)
+})
--- src/database/mysql.ts
+++ src/database/mysql.ts
@@ -2,47 +2,47 @@ import { createPool, Pool } from 'mysql2'
 import { Pool as PromisePool } from 'mysql2/promise'
 import { EnvType } from '../domain/constant/env-type'
 import config from '../config/config'
-import * as process from "process";
+import * as process from 'process'
 import * as fs from 'fs'
-import * as path from 'path';
+import * as path from 'path'
 
-let pool: Pool;
+let pool: Pool
 if (process.env.NODE_ENV === EnvType.PRODUCTION) {
-    pool = createPool(
-        {
-            host: config.mysql.host,
-            database: config.mysql.database,
-            port: +config.mysql.port,
-            user: config.mysql.user,
-            password: config.mysql.password,
-            multipleStatements: true,
-            ssl: {
-                ca: fs.readFileSync(path.join(__dirname, '../certs/').concat('rds-ca-2019-eu-west-2.pem'), 'utf8'),
-                rejectUnauthorized: config.mysql.rejectUnauthorized
-            }
-        })
+  pool = createPool(
+    {
+      host: config.mysql.host,
+      database: config.mysql.database,
+      port: +config.mysql.port,
+      user: config.mysql.user,
+      password: config.mysql.password,
+      multipleStatements: true,
+      ssl: {
+        ca: fs.readFileSync(path.join(__dirname, '../certs/').concat('rds-ca-2019-eu-west-2.pem'), 'utf8'),
+        rejectUnauthorized: config.mysql.rejectUnauthorized
+      }
+    })
 } else if (process.env.NODE_ENV === EnvType.TEST) {
-    pool = createPool({
-        host: config.mysql.host,
-        database: config.mysql.database,
-        port: +config.mysql.port,
-        user: config.mysql.user,
-        password: config.mysql.password,
-        multipleStatements: true,
-        ssl: {
-            ca: fs.readFileSync(path.join(__dirname, '../certs/').concat('rds-ca-2019-eu-west-2.pem'), 'utf8'),
-            rejectUnauthorized: config.mysql.rejectUnauthorized
-        }
-    })
+  pool = createPool({
+    host: config.mysql.host,
+    database: config.mysql.database,
+    port: +config.mysql.port,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    multipleStatements: true,
+    ssl: {
+      ca: fs.readFileSync(path.join(__dirname, '../certs/').concat('rds-ca-2019-eu-west-2.pem'), 'utf8'),
+      rejectUnauthorized: config.mysql.rejectUnauthorized
+    }
+  })
 } else {
-    pool = createPool({
-        host: config.mysql.host,
-        database: config.mysql.database,
-        port: +config.mysql.port,
-        user: config.mysql.user,
-        password: config.mysql.password,
-        multipleStatements: true
-    })
+  pool = createPool({
+    host: config.mysql.host,
+    database: config.mysql.database,
+    port: +config.mysql.port,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    multipleStatements: true
+  })
 }
 const mysqlPool: PromisePool = pool.promise()
 
