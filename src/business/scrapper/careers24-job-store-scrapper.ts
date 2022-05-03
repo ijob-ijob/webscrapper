c2e86cbbeb30ea4115be296ead3b78a71cd5f51f
adevnamednathi <maphanga.ng@gmail.com>
2022-05-04 01:42:26 +0200

    more clean up

--- src/business/scrapper/careers24-job-store-scrapper.ts
+++ src/business/scrapper/careers24-job-store-scrapper.ts
@@ -1,76 +1,69 @@
 import { PlatformType } from '../../domain/constant/platform-type'
-import { JobStoreRepo } from '../../database/job-store-repo'
-import { JobStoreEntity } from '../../domain/entities/job-store'
-import { Platform } from '../../domain/entities/platform'
-import { PlatformRepo } from '../../database/platform-repo'
-import { JobStoreStatusType } from '../../domain/constant/job-store-status-type'
-import * as puppeteer from 'puppeteer';
+import * as puppeteer from 'puppeteer'
 
 export class Careers24JobStoreScrapper {
     platform = PlatformType.CAREERS24;
     private url = 'https://www.careers24.com/'
 
-    async getLinks(): Promise<string[]> {
+    public async getLinks (): Promise<string[]> {
+      const browser = await puppeteer.launch({
+        args: ['--disable-dev-shm-usage'],
+        headless: true
+      })
+      const page = await browser.newPage()
+      await page.goto(this.url)
 
-        const browser = await puppeteer.launch({
-            args: ['--disable-dev-shm-usage'],
-            headless: true
-        })
-        const page = await browser.newPage()
-        await page.goto(this.url);
+      const btnSearchSelector = '#btnSearch'
+      await page.waitForSelector(btnSearchSelector)
+      await page.click(btnSearchSelector)
 
-        const btnSearchSelector = '#btnSearch'
-        await page.waitForSelector(btnSearchSelector)
-        await page.click(btnSearchSelector)
+      await page.waitForSelector('#pagination')
+      const totPages = await page.evaluate('document.querySelector("#pagination").getAttribute("data-total-pages")');
 
-        await page.waitForSelector('#pagination');
-        const totPages = await page.evaluate('document.querySelector("#pagination").getAttribute("data-total-pages")');
+      const linkAccum: string[] = []
 
-        let linkAccum: string[] = [];
-
-        await (async () => {
-            for (let i = 0; i < totPages; i++) {
-                await page.waitForSelector("#divSearchResults");
-                let unfilteredLinksList = await this.getPageLinks(page)
-                for (let j = 0; j < unfilteredLinksList.length; j++) {
-                    if (!linkAccum.includes(unfilteredLinksList[j])) {
-                        linkAccum.push(unfilteredLinksList[j]);
-                    }
-                }
-
-                const nextPageClickSelector = 'li.page-item:nth-child(6) > a:nth-child(1)'
-
-                await page.waitForXPath("/html/body/section/section/main/div[4]/div[3]/div[2]/div[4]/nav/ul/li[6]/a");
-                await page.click(nextPageClickSelector)
+      await (async () => {
+        for (let i = 0; i < totPages; i++) {
+          await page.waitForSelector('#divSearchResults')
+          const unfilteredLinksList = await this.getPageLinks(page)
+          for (let j = 0; j < unfilteredLinksList.length; j++) {
+            if (!linkAccum.includes(unfilteredLinksList[j])) {
+              linkAccum.push(unfilteredLinksList[j])
             }
-            await page.close()
-            await browser.close()
-        })()
+          }
 
-        return linkAccum
+          const nextPageClickSelector = 'li.page-item:nth-child(6) > a:nth-child(1)'
+
+          await page.waitForXPath('/html/body/section/section/main/div[4]/div[3]/div[2]/div[4]/nav/ul/li[6]/a')
+          await page.click(nextPageClickSelector)
+        }
+        await page.close()
+        await browser.close()
+      })()
+
+      return linkAccum
     }
 
-    private async getPageLinks(page: puppeteer.Page) {
-        const divSearchResultsSelector = 'divSearchResults';
-        return await page.evaluate((divSearchResultsSelector) => {
-            const links: string[] = [];
+    private async getPageLinks (page: puppeteer.Page) {
+      const divSearchResultsSelector = 'divSearchResults'
+      return await page.evaluate((divSearchResultsSelector) => {
+        const links: string[] = []
 
-            /**
-             * div.job-card -> div.row -> div.job-card-head -> aref
-             */
-            const divSearchResults = document.getElementById(divSearchResultsSelector);
+        /**
+         * div.job-card -> div.row -> div.job-card-head -> aref
+         */
+        const divSearchResults = document.getElementById(divSearchResultsSelector)
 
-            const eLinks = divSearchResults.querySelectorAll('a');
-            for (let i = 0; i < eLinks.length; i++) {
-                let iElement = eLinks[i].href;
+        const eLinks = divSearchResults.querySelectorAll('a')
+        for (let i = 0; i < eLinks.length; i++) {
+          const iElement = eLinks[i].href
 
-                if (iElement.includes('adverts') && !links.includes(eLinks[i].href)) {
-                    links.push(eLinks[i].href);
-                }
-            }
+          if (iElement.includes('adverts') && !links.includes(eLinks[i].href)) {
+            links.push(eLinks[i].href)
+          }
+        }
 
-            return links;
-        }, divSearchResultsSelector);
+        return links
+      }, divSearchResultsSelector)
     }
-
 }
